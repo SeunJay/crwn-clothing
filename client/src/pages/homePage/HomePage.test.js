13bad9461c244265ff9f098518d7c1c0611a4097
seun jay <seunjay92@gmail.com>
2020-08-01 18:32:00 +0100

    delete test files

--- client/src/pages/homePage/HomePage.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { shallow } from "enzyme";
-import HomePage from "./HomePage"
-
-it("should render the SignUnAndSignUp component", () => {
-  const wrapper = shallow(<HomePage />);
-
-  expect(wrapper).toMatchSnapshot();
-});
